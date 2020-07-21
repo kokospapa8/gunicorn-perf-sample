c87a5d2b0a1ed6a1873382081e881e920c80477b
jinwookbaek <nujabes8@mymusictaste.com>
2020-07-21 12:46:43 +0900

    locust initial commit

---
+++ locust/README.md
@@ -0,0 +1,8 @@
+```
+docker-compose run locust-standalone locust --host http://localhost -f tests/polls.py --no-web --clients 300 --hatch-rate 300 --run-time 10s --only-summary
+
+docker-compose run locust-standalone locust --host http://localhost -f tests/polls.py --no-web --clients 100 --hatch-rate 100 --run-time 60s --only-summary --step-load --step-clients 100 --step-time 20s
+
+```
+
+# docker-compose up --scale worker=4
---
+++ locust/tests/locustfile.py
@@ -0,0 +1,22 @@
+from random import randint
+
+from locust import User, TaskSet, between, task, HttpUser
+
+
+class ReadPosts(TaskSet):
+    @task(1)
+    def read_posts(self):
+        response = self.client.get("/api/v1/posts/", name="list posts")
+        print("Response status code:", response.status_code)
+        print("Response content:", response.text)
+
+        # csrftoken = response.cookies["csrftoken"]
+        # self.client.post(
+        #     "/polls/1/vote/",
+        #     {"choice": str(randint(1, 4))},
+        #     headers={"X-CSRFToken": csrftoken},
+        # )
+
+class WebsiteUser(HttpUser):
+    tasks = [ReadPosts]
+    wait_time = between(1.0, 2.0)
