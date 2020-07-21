50d606b82e9484996f58bd881bcf388ec5cb2eba
jinwookbaek <nujabes8@mymusictaste.com>
2020-07-21 22:41:32 +0900

    added worker number

--- app/config/app/django_app.sh
+++ app/config/app/django_app.sh
@@ -3,4 +3,4 @@ python manage.py migrate
 
 # Start gunicorn server at port 8000 and keep an eye for app code changes
 # If changes occur, kill worker and start a new one
-gunicorn --reload django-sample.wsgi:application -b 0.0.0.0:8000
+gunicorn --reload django-sample.wsgi:application -w ${WORKER_COUNT} -b 0.0.0.0:8000
