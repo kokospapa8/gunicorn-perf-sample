a4e7deb60937441e5ec34a8784840d436c57f9ca
jinwookbaek <nujabes8@mymusictaste.com>
2020-07-25 13:02:57 +0900

    added threads options

--- app/README.md
+++ app/README.md
@@ -3,6 +3,10 @@ ENV
 DB_PASSWORD
 DB_HOST
 WORKER_COUNT
-
-
 ```
+export WORKER_COUNT=5
+export TH_COUNT=1
+docker-compose -f docker-compose-staging.yml up -d
+
+$ ps aux | grep guni
+$ top
--- app/config/app/django_app.sh
+++ app/config/app/django_app.sh
@@ -4,4 +4,5 @@ python manage.py migrate
 # Start gunicorn server at port 8000 and keep an eye for app code changes
 # If changes occur, kill worker and start a new one
 echo worker count = ${WORKER_COUNT}
-gunicorn --reload django-sample.wsgi:application -w ${WORKER_COUNT} -b 0.0.0.0:8000
+echo th count = ${TH_COUNT}
+gunicorn --reload django-sample.wsgi:application -w ${WORKER_COUNT} -threads ${TH_COUNT} -b 0.0.0.0:8000
